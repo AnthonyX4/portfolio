d12e96dd8dc68bc53e1a76e15cb16d77c31f8d68
AnthonyX4 <62820583+AnthonyX4@users.noreply.github.com>
2025-08-21 22:29:11 -0400

    Refactor some animations to use framer motion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,17 +5,19 @@ import { BsFilePerson } from "react-icons/bs";
 import { LiaTimesSolid } from "react-icons/lia";
 import { FaBars } from "react-icons/fa6";
 import { useState } from "react";
+import { motion} from "framer-motion";
 
 const NavIcon = ({ link, children }) => {
   return (
-    <a
+    <motion.a
       href={link}
       target="_blank"
       rel="noopener noreferrer"
       className="navIcon"
+      whileHover={{ color: "var(--accent-color)" }}
     >
       {children}
-    </a>
+    </motion.a>
   );
 };
 
@@ -28,38 +30,41 @@ const Navbar = () => {
 
   return (
     <div>
-      <button 
-      id="toggle-sidebar-button"
-      onClick={() => toggleSidebar(isExpanded)}
-      className={isExpanded ? "open" : ""}
+      <button
+        id="toggle-sidebar-button"
+        onClick={() => toggleSidebar(isExpanded)}
       >
-        {isExpanded ? <LiaTimesSolid size={"1.5rem"} /> : <FaBars size={"1.5rem"}/>}
+        {isExpanded ? (
+          <LiaTimesSolid size={"1.5rem"} />
+        ) : (
+          <FaBars size={"1.5rem"} />
+        )}
       </button>
 
-      <nav
-        className={isExpanded ? "open" : ""}
-      >
-        <div className="navList">
-          <a className="logo">AT</a>
-          <a>About</a>
-          <a>Projects</a>
-          <a>Skills</a>
-          <a>Experience</a>
-          <a>Contact</a>
-        </div>
+        <nav 
+           className={isExpanded ? "open" : ""}
+        >
+          <div className="navList">
+            <a className="logo">AT</a>
+            <a>About</a>
+            <a>Projects</a>
+            <a>Skills</a>
+            <a>Experience</a>
+            <a>Contact</a>
+          </div>
 
-        <div className="navIcons">
-          <NavIcon link="https://github.com/AnthonyX4">
-            <BsGithub size={"1.4rem"} />
-          </NavIcon>
-          <NavIcon link="https://www.linkedin.com/in/anthony-truong-172374143/">
-            <BsLinkedin size={"1.4rem"} />
-          </NavIcon>
-          <NavIcon link="https://www.linkedin.com/in/anthony-truong-172374143/">
-            <BsFilePerson size={"1.4rem"} />
-          </NavIcon>
-        </div>
-      </nav>
+          <div className="navIcons">
+            <NavIcon link="https://github.com/AnthonyX4">
+              <BsGithub size={"1.4rem"} />
+            </NavIcon>
+            <NavIcon link="https://www.linkedin.com/in/anthony-truong-172374143/">
+              <BsLinkedin size={"1.4rem"} />
+            </NavIcon>
+            <NavIcon link="https://www.linkedin.com/in/anthony-truong-172374143/">
+              <BsFilePerson size={"1.4rem"} />
+            </NavIcon>
+          </div>
+        </nav>
     </div>
   );
 };
